f0c734669503f260c5000ab1a8988622d3ab4489
Martin Larralde <martin.larralde@embl.de>
2022-01-15 21:09:29 +0100

    Add `Join` trait for joining an iterable of `Display` items with a separator

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,7 @@
+use std::fmt::Display;
+use std::fmt::Write;
+use std::iter::Iterator;
+
 macro_rules! named_enum {
     ($(#[$attributes:meta])*
         pub enum $name:ident {
@@ -17,4 +21,25 @@ macro_rules! named_enum {
             }
         }
     };
+}
+
+pub trait Join {
+    fn join(&mut self, separator: &str) -> String;
+}
+
+impl<I> Join for I
+where
+    I: Iterator,
+    I::Item: Display,
+{
+    fn join(&mut self, separator: &str) -> String {
+        let mut joined = String::new();
+        for (i, item) in self.enumerate() {
+            if i > 0 {
+                joined.push_str(separator);
+            }
+            write!(joined, "{}", item).unwrap();
+        }
+        joined
+    }
 }
